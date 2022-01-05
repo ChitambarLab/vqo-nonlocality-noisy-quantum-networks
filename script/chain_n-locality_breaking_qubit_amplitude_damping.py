42f0411e806955948ee7c677d3ade2624f37cef7
Brian Doolittle <brian.d.doolittle@gmail.com>
2022-01-04 18:56:45 -0600

    amp damp data collection

--- script/chain_n-locality_breaking_qubit_amplitude_damping.py
+++ script/chain_n-locality_breaking_qubit_amplitude_damping.py
@@ -7,9 +7,10 @@ from context import QNetOptimizer as QNopt
 import utilities
 import network_ansatzes as ansatzes
 
-
 """
 This script collects data from noisy n-local chain optimizations.
+The considered noise model is an amplitude damping channel on a
+single qubit in the chain.
 
 The scan range is gamma in [0,1] with an interval of 0.05.
 Chains of length n = 2 and 3 are considered.
@@ -19,11 +20,11 @@ with local qubit rotations and a arbitrary two-qubit unitary on the central
 measurement node. 
 """
 
-def single_qubit_depolarizing_nodes_fn(n, wires=[0]):
+def single_qubit_amplitude_damping_nodes_fn(n, wires=[0]):
     def noise_nodes(noise_args):
         return [
             QNopt.NoiseNode(
-                wires, lambda settings, wires: qml.DepolarizingChannel(noise_args, wires=wires[0])
+                wires, lambda settings, wires: qml.AmplitudeDamping(noise_args, wires=wires[0])
             ),
         ]
 
@@ -37,13 +38,13 @@ if __name__ == "__main__":
         print("n = ", n)
         param_range = np.arange(0, 1.01, 0.05)
 
-        # local rotations on qubits
+        # local qubit rotation measurements
         time_start = time.time()
 
         local_rot_optimization = utilities.noisy_net_opt_fn(
             ansatzes.nlocal_max_entangled_prep_nodes(n),
             ansatzes.chain_local_rot_meas_nodes(n),
-            single_qubit_depolarizing_nodes_fn(n),
+            single_qubit_amplitude_damping_nodes_fn(n),
             QNopt.nlocal_chain_cost_22,
             opt_kwargs = {
                 "sample_width" : 5,
@@ -56,7 +57,7 @@ if __name__ == "__main__":
         local_rot_opt_dicts = client.gather(local_rot_jobs)
 
         utilities.save_optimizations_one_param_scan(
-            "script/data/chain_n-local_1-qubit_depolarizing/",
+            "script/data/chain_n-local_1-qubit_amplitude_damping/",
             "max_entangled_local_rot_n-" + str(n) + "_",
             param_range,
             local_rot_opt_dicts,
@@ -74,12 +75,12 @@ if __name__ == "__main__":
         bell_optimization = utilities.noisy_net_opt_fn(
             ansatzes.nlocal_max_entangled_prep_nodes(n),
             ansatzes.chain_bell_meas_nodes(n),
-            single_qubit_depolarizing_nodes_fn(n),
+            single_qubit_amplitude_damping_nodes_fn(n),
             QNopt.nlocal_chain_cost_22,
             opt_kwargs = {
                 "sample_width" : 5,
-                "step_size" : 0.7,
-                "num_steps" : 25,
+                "step_size" : 0.6,
+                "num_steps" : 30,
                 "verbose" : False,
             }
         )
@@ -87,7 +88,7 @@ if __name__ == "__main__":
         bell_opt_dicts = client.gather(bell_jobs)
 
         utilities.save_optimizations_one_param_scan(
-            "script/data/chain_n-local_1-qubit_depolarizing/",
+            "script/data/chain_n-local_1-qubit_amplitude_damping/",
             "max_entangled_bell_n-" + str(n) + "_",
             param_range,
             bell_opt_dicts,
@@ -105,12 +106,12 @@ if __name__ == "__main__":
         arb_optimization = utilities.noisy_net_opt_fn(
             ansatzes.nlocal_max_entangled_prep_nodes(n),
             ansatzes.chain_arb_meas_nodes(n),
-            single_qubit_depolarizing_nodes_fn(n),
+            single_qubit_amplitude_damping_nodes_fn(n),
             QNopt.nlocal_chain_cost_22,
             opt_kwargs = {
                 "sample_width" : 5,
-                "step_size" : 0.7,
-                "num_steps" : 25,
+                "step_size" : 0.6,
+                "num_steps" : 30,
                 "verbose" : False,
             }
         )
@@ -118,7 +119,7 @@ if __name__ == "__main__":
         arb_opt_dicts = client.gather(arb_jobs)
 
         utilities.save_optimizations_one_param_scan(
-            "script/data/chain_n-local_1-qubit_depolarizing/",
+            "script/data/chain_n-local_1-qubit_amplitude_damping/",
             "max_entangled_arb_n-" + str(n) + "_",
             param_range,
             arb_opt_dicts,
@@ -128,6 +129,5 @@ if __name__ == "__main__":
 
         time_elapsed = time.time() - time_start
         print("\nelapsed time : ", time_elapsed, "\n")
-
 
         client.restart()
