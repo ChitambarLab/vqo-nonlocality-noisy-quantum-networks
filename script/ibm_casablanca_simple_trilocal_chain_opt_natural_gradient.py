b1a82b5ab3dce6df727f7dd606c9470f679111ce
Brian Doolittle <brian.d.doolittle@gmail.com>
2022-01-30 08:03:22 -0600

    trilocal chain hardware parameters shift data

--- script/ibm_casablanca_simple_trilocal_chain_opt_natural_gradient.py
+++ script/ibm_casablanca_simple_trilocal_chain_opt_natural_gradient.py
@@ -9,30 +9,28 @@ import sys
 
 
 """
-Performs an optimization of the Trilocal Chain inequality on the IBM
+Performs an optimization of the Trilocal chain inequality on the IBM
 Casa Blanca quantum computer.
 The ansatz prepares maximally entangled states and optimizes
 local RY rotations on each measurement qubit.
-This ansatz can realize the maximal violation in noiseless settings.
 
 Positional Command Line Arguments:
 
     * [1] tmp_file_name: the file name of the tmp file to iterate
         upon, e.g., "2022-01-09T16-06-44Z.json".
-    * [2] num_steps: The number of steps to iterate upon the passed
-        in file. This parameter is only used if a file is passed in too.
+    * [2] current_step: Int, the current step in the optimization (first step is 0).
 
 """
 
 provider = IBMQ.load_account()
 provider = IBMQ.get_provider(hub="ibm-q-startup", group="xanadu", project="reservations")
 
-
 prep_nodes = [
     QNopt.PrepareNode(1, [0, 1], QNopt.ghz_state, 0),
     QNopt.PrepareNode(1, [2, 3], QNopt.ghz_state, 0),
     QNopt.PrepareNode(1, [4, 5], QNopt.ghz_state, 0),
 ]
+
 meas_nodes = [
     QNopt.MeasureNode(2, 2, [0], QNopt.local_RY, 1),
     QNopt.MeasureNode(2, 2, [1, 2], QNopt.local_RY, 2),
@@ -40,30 +38,35 @@ meas_nodes = [
     QNopt.MeasureNode(2, 2, [5], QNopt.local_RY, 1),
 ]
 
-trilocal_ansatz = QNopt.NetworkAnsatz(prep_nodes, meas_nodes)
+"""
+Theoretical execution (shots undefined) works for the natural gradient,
+but when shots is defined, the natural gradient does not optimize well.
 
+If this trend continues for the star network, we should discard natural
+gradient as a viable means of optimization.
+"""
 dev_ibm = {
-    "name": "qiskit.ibmq",
-    # "name" : "default.qubit",
-    "shots": 6000,
+    # "name": "qiskit.ibmq",
+    "name" : "default.qubit",
+    "shots": 24000,
     # "backend": "ibmq_qasm_simulator",
     # "backend": "ibmq_belem",
-    "backend": "ibmq_casablanca",
-    "provider": provider,
+    # "backend": "ibmq_casablanca",
+    # "backend" : "ibmq_bogota",
+    # "provider": provider,
 }
 
 ibm_ansatz = QNopt.NetworkAnsatz(prep_nodes, meas_nodes, dev_kwargs=dev_ibm)
 cost = QNopt.nlocal_chain_cost_22(ibm_ansatz, parallel=True, diff_method="parameter-shift")
-par_grad = QNopt.parallel_nlocal_chain_grad_fn(ibm_ansatz, diff_method="parameter-shift")
+nat_grad = QNopt.parallel_nlocal_chain_grad_fn(
+    ibm_ansatz, natural_gradient=False, diff_method="parameter-shift"
+)
 
-
-data_filepath = "script/data/ibm_casablanca_simple_trilocal_chain_opt_parameter_shift/"
-
+data_filepath = "script/data/ibm_casablanca_simple_trilocal_chain_opt_natural_gradient/"
 
 init_opt_dict = (
     QNopt.read_optimization_json(data_filepath + "tmp/" + sys.argv[1]) if len(sys.argv) > 1 else {}
 )
-
 
 num_steps = 10
 curr_step = 0 if len(sys.argv) <= 2 else int(sys.argv[2])
@@ -71,22 +74,21 @@ curr_step = 0 if len(sys.argv) <= 2 else int(sys.argv[2])
 print("init_opt_dict : ", init_opt_dict)
 print("num_steps : ", num_steps)
 
-
 opt_dict = utilities.hardware_opt(
     cost,
     ibm_ansatz.rand_scenario_settings(),
     num_steps=num_steps,
     current_step=curr_step,
-    step_size=1.6,
-    grad_fn=par_grad,
+    step_size=1,
+    grad_fn=nat_grad,
     tmp_filepath=data_filepath + "tmp/",
     init_opt_dict=init_opt_dict,
 )
 
-print(opt_dict)
+# print(opt_dict)
 
 
-# evaluating the score for the "theoretical" optimal settings
+# evaluating the score for the "theoretical" optimal settings on device
 opt_settings = [
     [np.array([[]]), np.array([[]]), np.array([[]])],  # prep settings
     [
@@ -101,7 +103,7 @@ opt_dict["theoretical_score"] = -(cost(opt_settings))
 opt_dict["device_name"] = dev_ibm["name"]
 opt_dict["device_shots"] = dev_ibm["shots"]
 
-print(opt_dict)
+# print(opt_dict)
 
 
 # saving data from optimization
@@ -111,7 +113,7 @@ filename = data_filepath + datetime_ext
 plt.plot(opt_dict["samples"], [np.sqrt(2)] * len(opt_dict["samples"]), label="Quantum Bound")
 plt.plot(opt_dict["samples"], [1] * len(opt_dict["samples"]), label="Classical Bound")
 plt.plot(opt_dict["samples"], opt_dict["scores"], label="Trilocal Chain Optimization")
-plt.title("IBM Hardware Parameter-Shift Optimization of\nTrilocal Chain Violation with Simple Ansatz")
+plt.title("IBM Hardware Parameter-Shift Optimization of\nTrilocl Chain Violation with Simple Ansatz")
 plt.ylabel("Trilocal Chain Score")
 plt.xlabel("Epoch")
 plt.legend()
