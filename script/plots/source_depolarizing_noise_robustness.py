12e860d79adad7c07360e0c515b7ac054645a07f
Brian Doolittle <brian.d.doolittle@gmail.com>
2022-04-17 22:32:25 -0500

    moving data

--- script/plots/source_depolarizing_noise_robustness.py
+++ script/plots/source_depolarizing_noise_robustness.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
 
     src.plot_unital_single_and_uniform_max_scores_data(
         fig_title = "Source Depolarizing Noise Robustness",
-        ax_titles = ["Single Qubit Noise", "Uniform Qubit Noise"],
+        ax_titles = ["Single Source Noise", "Uniform Source Noise"],
         noise_params = chsh_dep_data[0]["noise_params"],
         quantum_bound = np.sqrt(2),
         classical_bound = 1,
