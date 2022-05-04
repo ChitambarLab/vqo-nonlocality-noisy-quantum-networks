e04c994e3c751696f788c24587ca401d7d4cb45c
Brian Doolittle <brian.d.doolittle@gmail.com>
2022-05-04 16:51:53 -0500

    adding docs to maximal qubit violations

--- src/maximal_qubit_violations.py
+++ src/maximal_qubit_violations.py
@@ -3,7 +3,15 @@ import pennylane as qml
 
 
 def chsh_violation_criterion(operator):
+    """Collect the data needed to use the necessary conditions
+    for violation of the CHSH inequality.
 
+    :param operator: A matrix representing a two-qubit density operator.
+    :type operator: np.array
+
+    :returns: A triple containing the correlation matrix `corr_mat`, the symmetric 
+              correlation matrix product `U`, an the eigenvalues of U
+    """
     X = np.array([[0, 1], [1, 0]])
     Y = np.array([[0, -1j], [1j, 0]])
     Z = np.array([[1, 0], [0, -1]])
@@ -28,12 +36,17 @@ def chsh_violation_criterion(operator):
 
 
 def chsh_max_violation(rho):
+    """Returns the maximal violation of the density operator rho.
+    """
     (T, U, eigvals) = chsh_violation_criterion(rho)
 
     return 2 * np.sqrt(eigvals[-1] + eigvals[-2])
 
 
 def bilocal_max_violation_chsh_prod(rho1, rho2):
+    """Returns the square root of the product
+    of CHSH violations for state rho1 and rho2.
+    """
 
     (T1, U1, eigvals1) = chsh_violation_criterion(rho1)
     (T2, U2, eigvals2) = chsh_violation_criterion(rho2)
@@ -42,6 +55,9 @@ def bilocal_max_violation_chsh_prod(rho1, rho2):
 
 
 def bilocal_max_violation(rho1, rho2):
+    """Returns the max bilocal violation for states rho1 and rho2
+    using the Horodecki-like violation criterion.
+    """
 
     (T1, U1, eigvals1) = chsh_violation_criterion(rho1)
     (T2, U2, eigvals2) = chsh_violation_criterion(rho2)
@@ -50,6 +66,9 @@ def bilocal_max_violation(rho1, rho2):
 
 
 def star_max_violation_chsh_prod(states):
+    """Returns the n-th root of the product of CHSH violations for all
+    provided state.
+    """
 
     n = len(states)
 
@@ -59,6 +78,9 @@ def star_max_violation_chsh_prod(states):
 
 
 def star_max_violation(states):
+    """Returns the max star violation for the set of states using the
+    Horodecki-like violation criterion.
+    """
 
     n = len(states)
 
@@ -74,6 +96,10 @@ def star_max_violation(states):
 
 
 def chain_max_violation_chsh_prod(states):
+    """Returns the n-th root of the product of CHSH violations for all
+    provided states.
+    """
+
     n = len(states)
 
     chsh_violations = [chsh_max_violation(state) / 2 for state in states]
@@ -81,17 +107,13 @@ def chain_max_violation_chsh_prod(states):
     return np.power((np.prod(chsh_violations)), 1 / n)
 
 
-def chain_max_violation_chsh_prod2(states):
-    n = len(states)
-
-    chsh_violations = [chsh_max_violation(state) / 2 for state in states]
-
-    S_bilocal = bilocal_max_violation_chsh_prod(states[0], states[-1])
-
-    return np.power((S_bilocal ** 2) * np.prod(chsh_violations), 1 / n)
-
-
 def chain_classical_interior_max_violation(states):
+    """Returns the bilocal score for the first and last states
+    computed as the sqrt of CHSH violations. The remaining interior 
+    states are evaluated as classical strategies using only their largest
+    eigenvalue.
+    """
+
     n = len(states)
 
     S_bilocal = bilocal_max_violation_chsh_prod(states[0], states[-1])
@@ -102,6 +124,9 @@ def chain_classical_interior_max_violation(states):
 
 
 def chain_max_violation(states):
+    """Returns the max chain violation for the set of states using the
+    Horodecki-like violation criterion.
+    """
 
     n = len(states)
 
@@ -112,37 +137,3 @@ def chain_max_violation(states):
     states_eigvals2 = [eigvals[-2] for eigvals in states_eigvals]
 
     return np.sqrt(np.sqrt(np.prod(states_eigvals1)) + np.sqrt(np.prod(states_eigvals2)))
-
-
-if __name__ == "__main__":
-
-    @qml.qnode(qml.device("default.mixed", wires=[0, 1]))
-    def circ():
-        # qml.Hadamard(wires=[0])
-        # qml.CNOT(wires=[0,1])
-
-        # qml.Rot(0.1,0.2,0.3, wires=[0])
-
-        # qml.AmplitudeDamping(0.3, wires=[0])
-        qml.PhaseDamping(0.3, wires=[0])
-
-        return qml.state()
-
-    print(
-        chsh_violation_criterion(
-            np.array([[4 / 3, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) / 2
-            + np.eye(4) / 12
-        )
-    )
-
-    # state = np.array([[1,0,0,1],[0,0,0,0],[0,0,0,0],[1,0,0,1]])/2
-    state = circ()
-
-    print(state)
-    (T, U, eigenvals) = chsh_violation_criterion(state)
-
-    print(T)
-    print(U)
-    print(eigenvals)
-
-    print(chsh_max_violation(state))
